7561f53b51311f28670ec2f513db632e4bd96e33
Léo S <56728067+LeoSarochar@users.noreply.github.com>
2021-04-29 08:50:05 +0200

    Update main.py

--- ex04/main.py
+++ ex04/main.py
@@ -2,7 +2,8 @@ s = 'radkar'
 def palyndrom(s):
     for i in range(len(s)):
         t = s[:i] + s[i+1:]
-        if t == t[::-1]: return True
+        if t == t[::-1]:
+            return True
 
     return s == s[::-1]
 
